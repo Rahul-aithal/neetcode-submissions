f1ac0b8b5dff87443b436a6f8cdc72e4e04a4af3
Rahul aithal <aithalrahul34@gmail.com>
2026-07-26 18:47:47 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        backtrack(0, nums, subset, res);
+        return res;
+    }
+    private void backtrack(int i, int[] nums, List<Integer> subset, List<List<Integer>> res) {
+        if (i >= nums.length) {
+            res.add(new ArrayList<>(subset));
+
+            return;
+        }
+        // 1'st add this item;
+        subset.add(nums[i]);
+        backtrack(i + 1, nums,subset,res);
+        // 2nd don't add this item;
+    
+            subset.removeLast();
+
+        backtrack(i + 1, nums,subset,res);
+    }
+}
