693e0a3cc0984e58f1b50283688555ae6f0fa807
Rahul aithal <aithalrahul34@gmail.com>
2026-08-03 22:54:42 +0530

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.java
@@ -0,0 +1,33 @@
+class Solution {
+    List<List<String>> res = new ArrayList<>();
+    public List<List<String>> partition(String s) {
+        List<String> sub = new ArrayList<>();
+        dfs(0,s,sub);
+        return res;
+    }
+
+    public void dfs(int i,String s,List<String> ls){
+        if(i>=s.length()){
+            res.add(new ArrayList<>(ls));
+            return;
+        }
+
+        for(int j=i;j<s.length();j++){
+            if(isPali(s,i,j)){
+                ls.add(s.substring(i,j+1));
+                dfs(j+1,s,ls);
+                ls.remove(ls.size()-1);
+            }
+        }
+    }
+
+    public boolean isPali(String s,int l,int r){
+        while(l<r){
+            if(s.charAt(l++)!=s.charAt(r--)){
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
