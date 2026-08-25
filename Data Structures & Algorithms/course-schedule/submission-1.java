47a36da3f4f0480c0f20c55bf1222e679d1236bb
Rahul aithal <aithalrahul34@gmail.com>
2026-08-25 15:52:03 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.java
@@ -0,0 +1,45 @@
+class Solution {
+    Set<Integer> completed = new HashSet<>();
+    int[] visited;
+    Map<Integer, List<Integer>> mapPQ;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int pl = prerequisites.length;
+        this.visited = new int[numCourses];
+        this.mapPQ = new HashMap<>();
+
+        for (int i = 0; i < pl; i++) {
+         mapPQ.put(prerequisites[i][0], new ArrayList<>());
+        }
+        for (int i = 0; i < pl; i++) {
+            List<Integer> req = mapPQ.get(prerequisites[i][0]);
+            req.add(prerequisites[i][1]);
+        }
+       
+        for (int c = 0; c < numCourses; c++) {
+            if (completed.contains(c))
+                continue;
+            if (!add(c, prerequisites)) {
+                return false;
+            };
+        }
+        return completed.size() == numCourses;
+    }
+
+    public boolean add(int c, int[][] pq) {
+        if (completed.contains(c))
+            return true;
+        if (visited[c] == 1)
+            return false;
+        List<Integer> req = mapPQ.get(c);
+        visited[c] = 1;
+        if (req != null) {
+            for (int r : req) {
+                if (!add(r, pq)) {
+                    return false;
+                }
+            }
+        }
+        completed.add(c);
+        return true;
+    }
+}
